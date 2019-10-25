304235e376334cb21055f255825f1b1f87b6df6d
Mahmoud Mokhtar <mahmok@gmail.com>
2019-10-25 03:23:55 +0200

    Updating base url

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'todo-list',
     environment,
-    rootURL: '/',
+    rootURL: '',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
